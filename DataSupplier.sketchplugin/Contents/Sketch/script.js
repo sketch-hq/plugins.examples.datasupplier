6df6c450ebd2d163b463af8a6c45fa615f99982a
Kevin Meaney <ktam@yvs.eu.com>
2017-08-16 18:19:05 +0100

    Example data feed plugin updated to include images. Ref: #11936

--- DataSupplier.sketchplugin/Contents/Sketch/script.js
+++ DataSupplier.sketchplugin/Contents/Sketch/script.js
@@ -1,12 +1,23 @@
+
 var onStartup = function(context) {
   log('In DataSupplier onStartup');
   var sketch = context.api();
   
   var dataManager = sketch.dataManager();
   
-  // Register some static data available for random use.
+  // Register some static data available for use.
   var staticListOfPeople = ['Lucy', 'Johnnie', 'Petie', 'Jamie', 'Tina', 'Gillie', 'Tania', 'Peta', 'Rudolpho', 'Jellie', 'Ricki', 'Lori', 'Jorgi', 'Marki'];
   dataManager.registerStaticSupplier('public.text', 'First Names', staticListOfPeople);
+
+  // Register some static image data available for use.
+  var staticListOfPersonPicturesNames = ['0.jpg', '1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg'];
+  var arrayLength = staticListOfPersonPicturesNames.length;
+  var paths = [];
+  for (var i = 0 ; i < arrayLength ; i++) {
+    paths.push(sketch.resourceNamed(staticListOfPersonPicturesNames[i]).path())
+  }
+
+  dataManager.registerStaticSupplier('public.image', 'Faces', paths);
   
   // Register a method to supply random data on request.
   dataManager.registerDynamicSupplier('public.text', 'UK Regions', 'ukRegionsKey');
